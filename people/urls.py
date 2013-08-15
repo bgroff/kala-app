2dfe81eb28b16dfd2c0793e53e766e3a36dfaff4
Bryce Groff <bgroff@hawaii.edu>
2013-08-15 09:56:41 -1000

    - Remove the people app.

--- people/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from .views import PeopleView
-
-urlpatterns = patterns('',
-                       url(regex=r'^$',
-                           view=PeopleView.as_view(),
-                           name='people',
-                       ),
-)
