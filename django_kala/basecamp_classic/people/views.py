fbe6c0e1a54116a39d9b4296e6cd394ea6c7c3b5
Bryce Groff <bgroff@hawaii.edu>
2017-03-16 21:39:11 -0700

    * Remove unneeded import.

--- django_kala/basecamp_classic/people/views.py
+++ django_kala/basecamp_classic/people/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
@@ -5,7 +6,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 
-from django.contrib.auth import get_user_model
 
 from .parsers import XMLParser
 from .renderers import XMLPeopleRenderer
