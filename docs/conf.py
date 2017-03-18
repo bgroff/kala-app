22af5dbfb4bdb1fbd7356a2a1983331db408d823
Bryce Groff <bgroff@hawaii.edu>
2017-03-17 22:29:56 -0700

    * Use the default theme and see if rtd uses their own theme.

--- docs/conf.py
+++ docs/conf.py
@@ -79,23 +79,22 @@ todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
-import sphinx_rtd_theme
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'canonical_url': 'http://kala-app.readthedocs.io/en/latest/'}
+#html_theme_options = {'canonical_url': 'http://kala-app.readthedocs.io/en/latest/'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = []
 
 
 # -- Options for HTMLHelp output ------------------------------------------
