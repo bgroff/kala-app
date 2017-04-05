469569d7aa3cfe99138253e8e29c3e574674fa42
Bryce Groff <bgroff@hawaii.edu>
2017-04-04 19:24:21 -0700

    * Add the Category model to the Project app. This allows Documents to be associated with a category. * Rebuild the initial migration.

--- django_kala/projects/models.py
+++ django_kala/projects/models.py
@@ -43,3 +43,12 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    project = models.ForeignKey(Project)
+    type = models.CharField(max_length=20, db_index=True)
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.name, self.type)
