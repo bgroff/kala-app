10e442acffac82244749fc95219fa0398a2ea1e1
Bryce Groff <bgroff@hawaii.edu>
2017-12-17 11:03:14 -0800

    * Add failure pages so the user gets something nicer than nginx saying tough luck.

--- django_kala/django_kala/urls.py
+++ django_kala/django_kala/urls.py
@@ -1,21 +1,9 @@
-"""django_kala URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import url, include
+from django.views.defaults import page_not_found, permission_denied, server_error
+
 from .views import Home, SearchView, LicenseView, UserDocumentationView
+
 
 urlpatterns = [
     url(
@@ -72,4 +60,8 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
+        url(r'^404/$', page_not_found, {'exception': Exception()}),
+        url(r'^403/$', permission_denied, {'exception': Exception()}),
+        url(r'^500/$', server_error, ),
+
     ] + urlpatterns
