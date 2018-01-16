9f63fe4c90e769b25552668d60a8782baad75c19
Bryce Groff <bgroff@hawaii.edu>
2018-01-15 16:32:50 -0800

    * Add missing on_delete handler for the export model.

--- django_kala/projects/models.py
+++ django_kala/projects/models.py
@@ -144,5 +144,5 @@ class Category(models.Model):
 
 class Export(models.Model):
     details = JSONField(default={})
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     key = models.CharField(max_length=255)
