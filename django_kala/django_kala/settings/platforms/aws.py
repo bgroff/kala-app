db19686347dec05deddf01ece73372640c862853
Bryce Groff <bgroff@hawaii.edu>
2018-08-16 17:39:01 -0700

    * Add the AWS_REGION to the settings.

--- django_kala/django_kala/settings/platforms/aws.py
+++ django_kala/django_kala/settings/platforms/aws.py
@@ -2,3 +2,4 @@ from django_kala.settings import get_env_variable
 
 S3_MEDIA_URL = get_env_variable('S3_MEDIA_URL', '')
 S3_STORAGE_BUCKET = get_env_variable('S3_STORAGE_BUCKET', '')
+AWS_REGION = get_env_variable('AWS_REGION', 'us-west-1')
