b143be2ff1dc8d9c6c51cc50fd66b053865868c6
Bryce Groff <bgroff@hawaii.edu>
2018-11-07 19:19:04 -0800

    * Remove the type field from the category.

--- django_kala/projects/models.py
+++ django_kala/projects/models.py
@@ -152,7 +152,6 @@ class ProjectPermission(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=255)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    type = models.CharField(max_length=20, db_index=True, null=True, blank=True)
 
     def __str__(self):
         return '{0}'.format(self.name)
