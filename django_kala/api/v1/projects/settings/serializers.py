8b6dc22af9ac948c7c788ada4530c953d8ee2bcf
Bryce Groff <bgroff@hawaii.edu>
2019-06-18 21:21:17 -0700

    * Change the output of the api to have more structure. * Add the foundation for modifying permissions.

--- django_kala/api/v1/projects/settings/serializers.py
+++ django_kala/api/v1/projects/settings/serializers.py
@@ -3,14 +3,49 @@ from documents.models import DocumentPermission
 
 
 class DocumentPermissionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    user_id = serializers.IntegerField()
-    username = serializers.CharField()
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    document_id = serializers.IntegerField()
-    project_id = serializers.IntegerField()
-    organization_id = serializers.IntegerField()
-    document_permission = serializers.CharField()
-    project_permission = serializers.CharField()
-    organization_permission = serializers.CharField()
+    id = serializers.IntegerField(required=False)
+    user_id = serializers.IntegerField(required=True)
+    username = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    document_id = serializers.IntegerField(required=True)
+    document_name = serializers.IntegerField(required=False)
+    project_id = serializers.IntegerField(required=False)
+    project_name = serializers.IntegerField(required=False)
+    organization_id = serializers.IntegerField(required=False)
+    organization_name = serializers.IntegerField(required=False)
+    document_permission = serializers.ChoiceField(required=True,
+                                                  choices=[('can_invite', 'Can invite'), ('can_create', 'Can create'),
+                                                           ('can_manage', 'Can manage')])
+    project_permission = serializers.ChoiceField(required=False,
+                                                 choices=[('can_invite', 'Can invite'), ('can_create', 'Can create'),
+                                                          ('can_manage', 'Can manage')])
+    organization_permission = serializers.ChoiceField(required=False, choices=[('can_invite', 'Can invite'),
+                                                                               ('can_create', 'Can create'),
+                                                                               ('can_manage', 'Can manage')])
+
+    def to_representation(self, obj):
+        return {
+            'id': obj.id,
+            'user': {
+                'id': obj.user_id,
+                'username': obj.username,
+                'first_name': obj.first_name,
+                'last_name': obj.last_name
+            },
+            'document': {
+                'id': obj.document_id,
+                'name': obj.document_name,
+                'permission': obj.document_permission
+            },
+            'project': {
+                'id': obj.project_id,
+                'name': obj.project_name,
+                'permission': obj.project_permission
+            },
+            'organization': {
+                'id': obj.organization_id,
+                'name': obj.organization_name,
+                'permission': obj.organization_permission
+            }
+        }
--- django_kala/api/v1/projects/settings/views.py
+++ django_kala/api/v1/projects/settings/views.py
@@ -1,14 +1,20 @@
 from django.db import connections
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.renderers import JSONRenderer
+from rest_framework.exceptions import PermissionDenied, NotFound, APIException
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView, ListAPIView
+
 from psycopg2.extras import NamedTupleCursor
 
 
 from api.v1.projects.settings.serializers import DocumentPermissionSerializer
 from auth.models import User
+from documents.models import Document
+from projects.models import Project
+
 
 
 class ProjectsView(APIView):
@@ -28,9 +34,25 @@ class DocumentView(APIView):
 
 
 class DocumentPermissionsView(CreateAPIView, ListAPIView):
-    queryset = User.objects.all()
+    queryset = Document.objects.all()
     serializer_class = DocumentPermissionSerializer
     permission_classes = []
+
+    def dispatch(self, request, project_pk, document_pk, *args, **kwargs):
+        try:
+            self.project = get_object_or_404(Project.objects.select_related(), pk=project_pk)
+            self.document = get_object_or_404(Document, pk=document_pk)
+
+            if not self.document.can_manage(request.user):
+                raise PermissionDenied('You do not have permission to modify this documents permission')
+
+            if self.document.project.id != self.project.id:
+                raise NotFound('The document does not exist')
+
+            self.available_users = request.user.get_users().values_list('id', flat=True)
+            return super().dispatch(request, *args, **kwargs)
+        except Exception as e:
+            raise APIException(str(e))
 
     def list(self, request, *args, **kwargs):
         connection = connections['default']
@@ -43,8 +65,11 @@ class DocumentPermissionsView(CreateAPIView, ListAPIView):
                    first_name,
                    last_name,
                    kala_documents.id AS document_id,
-                   kala_projects.id  AS project_id, 
+                   kala_documents.name AS document_name,
+                   kala_projects.id  AS project_id,
+                   kala_projects.name  AS project_name,
                    kala_companies.id AS organization_id,
+                   kala_companies.name AS organization_name,
                    (select codename from auth_permission where id = documents_documentpermission.permission_id) as document_permission,
                    (select codename from auth_permission where id = projects_projectpermission.permission_id) as project_permission,
                    (select codename from auth_permission where id = organizations_organizationpermission.permission_id) as organization_permission
@@ -66,6 +91,6 @@ class DocumentPermissionsView(CreateAPIView, ListAPIView):
                              AND organizations_organizationpermission.user_id =
                                  documents_documentpermission.user_id
             WHERE document_id = %s OR kala_user.id in %s;
-            """, [2659, tuple(request.user.get_users().values_list('id', flat=True))])
+            """, [self.document.id, tuple(self.available_users)])
             s = DocumentPermissionSerializer(cursor.fetchall(), many=True)
             return Response(s.data)
