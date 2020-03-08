979de251a5031c3bbef14188b85a93329aa68ce5
Bryce Groff <brycegroff@gmail.com>
2020-03-07 18:54:53 -0800

    Fixup some of the settings.

--- django_kala/django_kala/settings/celery.py
+++ django_kala/django_kala/settings/celery.py
@@ -4,7 +4,7 @@ EXPORT_QUEUE = get_env_variable('EXPORT_QUEUE', default='exports')
 DELETE_QUEUE = get_env_variable('DELETE_QUEUE', default='deletes')
 
 CELERY_BROKER_URL = 'sqs://'
-CELERY_BROKER_TRANSPORT_OPTIONS = {'region': get_env_variable('AWS_REGION', default='us-west-2')}
+CELERY_BROKER_TRANSPORT_OPTIONS = {'region': get_env_variable('AWS_REGION', default='us-west-1')}
 CELERY_TASK_ROUTES = {
     'organizations.tasks.delete_organization.DeleteOrganizationTask': DELETE_QUEUE,
     'projects.tasks.delete_document.DeleteDocumentTask': DELETE_QUEUE,
--- django_kala/django_kala/settings/platforms/aws.py
+++ django_kala/django_kala/settings/platforms/aws.py
@@ -3,3 +3,4 @@ from django_kala.functions import get_env_variable
 S3_MEDIA_URL = get_env_variable('S3_MEDIA_URL', '')
 S3_STORAGE_BUCKET = get_env_variable('S3_STORAGE_BUCKET', '')
 AWS_REGION = get_env_variable('AWS_REGION', 'us-west-1')
+S3_STORAGE_PREFIX = get_env_variable('S3_STORAGE_PREFIX', '')
