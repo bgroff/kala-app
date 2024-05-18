6ace78968d0de9e359f08778d6c0243fe739a9b6
Bryce Groff <bgroff@hawaii.edu>
2024-05-18 13:00:54 -0700

    Parse the mail values into the correct data types.

--- django_kala/django_kala/settings/email.py
+++ django_kala/django_kala/settings/email.py
@@ -7,11 +7,13 @@ EMAIL_HOST_PASSWORD = get_env_variable('KALA_EMAIL_HOST_PASSWORD', default='')
 EMAIL_HOST_USER = get_env_variable('KALA_EMAIL_HOST_USER', default='')
 EMAIL_PORT = get_env_variable('KALA_EMAIL_PORT', default=25)
 EMAIL_SUBJECT_PREFIX = get_env_variable('KALA_EMAIL_SUBJECT_PREFIX', default='')
-EMAIL_USE_TLS = get_env_variable('KALA_EMAIL_USE_TLS', default=False)
-EMAIL_USE_SSL = get_env_variable('KALA_EMAIL_USE_SSL', default=False)
+EMAIL_USE_TLS = bool(get_env_variable('KALA_EMAIL_USE_TLS', default=False))
+EMAIL_USE_SSL = bool(get_env_variable('KALA_EMAIL_USE_SSL', default=False))
 EMAIL_SSL_CERTFILE = get_env_variable('KALA_EMAIL_SSL_CERTFILE', default=False)
 EMAIL_SSL_KEYFILE = get_env_variable('KALA_EMAIL_SSL_KEYFILE', default=False)
-EMAIL_TIMEOUT = get_env_variable('KALA_EMAIL_TIMEOUT', default=False)
+
+timeout = get_env_variable('KALA_EMAIL_TIMEOUT', default=None)
+EMAIL_TIMEOUT = int(timeout) if timeout is not None else None
 
 # Kala settings
 FROM_EMAIL = get_env_variable('KALA_FROM_EMAIL', default='kala@maloloindustries.com')
